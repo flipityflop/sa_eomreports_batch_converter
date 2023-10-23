49d5acb6af06dda0d9cf144dc113bf239b2a94d7
Shlomo Friedman <sz.friedman123@gmail.com>
2023-10-23 18:05:19 -0400

    tweaked exe and pandas parser fixed number formatting, tweaked errorhandling

--- dist/pause.bat
+++
@@ -1,2 +0,0 @@
-"SA EOM Conversion Tool v0.31.exe"
-pause
--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import xlsxwriter
 from matplotlib.backends.backend_pdf import PdfPages
-from sys import exit
+#if quitter() is changed back to exit(), make sure to uncomment out the following import
+#from sys import exit
 import os
 
+print("This tool was created by Shlomo Friedman. Reach out to him for more information or help.")
+
+def input_folder_OR_file(user_input_folder='', user_input_file='', filelist='', f='', 
+                         error_message_list=[]):
+    while True:
+        try: 
+            filelist = []
+            user_input_folder = input("What is the folder or file? [Provide the full path.]\n>> ").replace("\\", "/").strip('""')
+            try: 
+                for user_input_file in os.listdir(user_input_folder):
+                    if '.' not in user_input_file:
+                        f = user_input_folder + '/' + user_input_file
+                        filelist.append(f)
+                    else:
+                        f = user_input_folder + '/' + user_input_file
+                        error_message_list.append(f"{f} is not a supported filetype.")
+            except:
+                filelist.append(user_input_folder)
+
+            manage_dataframe(filelist, error_message_list)
+            
+        except WindowsError as err:
+            print(f"Error! {err}")
+            input_folder_OR_file()
 
 
-def input_file(user_input_file='', filelist=''):
-    filelist = []
-    user_input_file = input("What is the file? [Provide the full file path.]\n>> ").replace("\\", "/").strip('""')
-    filelist.append(user_input_file)
-    print(filelist)
-    manage_dataframe(filelist)
-
-def input_folder(user_input_folder='', user_input_file='', filelist='', f=''):
-    filelist = []
-    user_input_folder = input("What is the folder? [Provide the full folder path.]\n>> ").replace("\\", "/").strip('""')
-    for user_input_file in os.listdir(user_input_folder):
-        if '.' not in user_input_file:
-            f = user_input_folder + '/' + user_input_file
-            filelist.append(f)            
-
-    manage_dataframe(filelist)
-
-def manage_dataframe(filelist, user_input_file='', df='', columns='', column_count='', 
-                     filename='', infile='', error_message='', file_count='', error_message_list=[]):
+def manage_dataframe(filelist, error_message_list, user_input_file='', df='', columns='', column_count='', 
+                     filename='', infile='', file_count='', worksheet=''):
     
     file_count = 0
     while True:
@@ -34,30 +43,43 @@ def manage_dataframe(filelist, user_input_file='', df='', columns='', column_cou
                 print(f"Parsing {user_input_file}")
                 filename = user_input_file.rsplit('/', 1)[-1]
                 user_input_file = user_input_file.replace("\\", "/").strip('""')
-                df = pd.read_table(user_input_file, sep="|", 
+                #import table as pandas dataframe
+                df = pd.read_table(user_input_file, sep="|",
                                 skipinitialspace=True,
+                                thousands=',',
+                                parse_dates=[1],
+                                date_format="%m/%d/%y",
                                 index_col=[0], 
                                 float_precision='high', 
                                 skip_blank_lines=True,
                                 on_bad_lines='warn',
-                                na_filter=False)
+                                na_filter=True)
+                #removed trailing unamed column
                 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-                df = df.fillna("unkown")
+                #strip extra space in column name            
                 for i in df.columns:
-                    if df[i].dtype == 'object':
+                    #print(df[i].dtype)
+                    if df[i].dtype == 'object' and not 'float':
                         df[i] = df[i].map(str.strip)
                     else:
                         pass
+        
+                #print(df.values)
                 column_count = len(df.columns)
                 #create and write excel
                 print("Writing...")
-                df.to_excel(f"{user_input_file}.xlsx")
+                worksheet = (f"{user_input_file}.xlsx")
+                with pd.ExcelWriter(worksheet,
+                                    datetime_format="MM/DD/YY"
+                                ) as writer:
+                    df.to_excel(writer, index=False)
+                
                 print("Success!")
                 file_count += 1
 
             except pd.errors.ParserError:
-                error_message = f"Failed on {user_input_file}"
-                error_message_list.append(error_message)
+                error_message_list = []
+                error_message_list.append(f"Could not parse {user_input_file}")
                 print(f"Failed on {user_input_file}")
         quitter(error_message_list, file_count)   
 
@@ -80,20 +102,12 @@ def write_pdf(df, columns):
 #exit module
 def quitter(error_message_list, file_count):
     if error_message_list != []:
-        print(f"{file_count} files successfully rendered, {error_message_list}")
+        print(f"{file_count} files successfully rendered. Errors:")
+        for i in error_message_list:
+            print(i)
     else:
         print(f"All {file_count} files converted successfully!")
     input("Press ANY KEY to restart>> ")
-    fileORfolder()
+    input_folder_OR_file()
 
-def fileORfolder():
-    choice = input("1. for files or 2. for folders\n>> ")
-    if choice == '1':
-        input_file()
-    elif choice == '2':
-        input_folder()
-    else:
-        print("pick a number")
-        fileORfolder()
-
-fileORfolder()
+input_folder_OR_file()
